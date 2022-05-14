ab40126e2d1c6bd7486029e3781ef4790b9f431c
moises sanchez <moy5921@gmail.com>
2022-05-14 16:38:23 -0500

    4: añadiendo segundo componente y comportamiento responsive principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recolectaBasura';
+
+  tarjetas:any[] = [
+
+    {
+      titulo:"comunidad",
+      alt:"comunidad",
+      imagen:"../assets/img/social.svg"
+      
+    },
+    {
+      titulo:"impacto",
+      alt:"impacto",
+      imagen:"../assets/img/impacto.svg"
+    },
+    {
+      titulo:"beneficios",
+      alt:"beneficios",
+      imagen:"../assets/img/beneficio.svg"
+    }
+
+  ];
+
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { InfoGeneralComponent } from './info-general/info-general.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    InfoGeneralComponent
   ],
   imports: [
     BrowserModule,
