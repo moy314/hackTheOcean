eb19437e0a56da6de4a770b858605ee8cd74cf60
moises sanchez <moy5921@gmail.com>
2022-05-15 21:05:12 -0500

    11: agregando links

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 //import { link } from 'fs';
 
 @Component({
@@ -6,36 +7,56 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent  {
+export class HomeComponent  implements AfterViewInit{
 
-  constructor() { }
+  @ViewChild("problematica")
+  public problematica!: ElementRef;
 
+  public moveToStructure():void {
+}
+
+  constructor(private router: Router ) { 
+
+    this.moveToStructure();
+
+  }
+  ngAfterViewInit(): void {
+    console.log(this.problematica);
+
+  }
+
+  
   tarjetas:any[] = [
 
     {
       titulo:"Recoje",
       alt:"recoje",
       imagen:"../assets/img/recoje.svg",
-      link:"['problematica']"
+      link:"/home"
     },
     {
       titulo:"Canjea",
       alt:"canjea",
       imagen:"../assets/img/canjea.svg",
-      link:"['general']"
+      link:"/general"
 
     },
     {
       titulo:"Disfruta",
       alt:"disfruta",
       imagen:"../assets/img/disfruta.svg",
-      link:"['patrocinadores']"
+      link:"/patrocinadores"
     }
 
   ];
 
+  public rediredt(tarjeta: any) {
+    console.log(tarjeta.link)
+    this.router.navigate([tarjeta.link]);
+  }
 
-
-  
+  public gotoProblematica() {
+  this.problematica.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'start' });
+  }
 
 }
